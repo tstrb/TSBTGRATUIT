cec335def1bed825c69f48224a56c58ef91a6cba
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-02-11 12:04:12 +0300

    Create sdxl.js

---
+++ script/sdxl.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "sdxl",
+    aliases: ['sdx'],
+    author: "TsantaBot",
+    version: "2.0",
+    cooldowns: 60,
+    role: 0,
+    shortDescription: {
+      en: ""
+    },
+    longDescription: {
+      en: "generate an image sdxl"
+    },
+    category: "image",
+    guide: {
+      en: "[prompt | model]"
+    }
+  },
+  run: async function ({ api, event, args }) {
+    let path = __dirname + "/cache/image.png";
+    let prompt;
+    let model = 1;
+
+    if (args.length === 0) {
+      return api.sendMessage("▪︎ Code: sdxl [prompt] | [model] \n▪︎ Ex: sdxl Cat cyborg cyber punk | 2 \n\n TsantaBot: https://bit.ly/tsantabot", event.threadID, event.messageID);
+    }
+
+    if (args.length > 1) {
+      const tzt = args.join(" ").split("|").map(item => item.trim());
+      prompt = tzt[0];
+      model = tzt[1];
+    } else {
+      prompt = args[0];
+    }
+
+    let tid = event.threadID;
+    let mid = event.messageID;
+
+    try {
+      api.sendMessage("⏳ | TsantaBot_sdxl est en train d'imaginer votre texte... ", tid, mid);
+
+      let enctxt = encodeURIComponent(prompt);
+      let url = `http://ger2-1.deploy.sbs:1792/sdxl?prompt=${enctxt}&styles=${model}`;
+
+      let response = await axios.get(url, { responseType: "stream" });
+
+      response.data.pipe(fs.createWriteStream(path));
+
+      response.data.on("end", () => {
+        api.sendMessage({ attachment: fs.createReadStream(path) }, tid, () => fs.unlinkSync(path), mid);
+      });
+    } catch (e) {
+      return api.sendMessage(e.message, tid, mid);
+    }
+  }
+};

126d4d9d9510437100083a8392121b79dc05a342
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-02-11 11:18:25 +0300

    Update bing.js

--- script/bing.js
+++ script/bing.js
@@ -1,50 +1,74 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
-const KievRPSSecAuth = "FACSBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACGtoWFdkFVMtUATNj+B24yvDCqnHSWEG32Qnvt76AZ0/mNodKyZlz/rRXrrKBt5UbG4/FbZeiEKqydGDVzoLcXZg44NGf7tlB2XFtSNronA6piA5Y6gWIIwQmpIGdzKP6AbuPxz8pOJt7kP+6Mm5txHZAVB+nqfvy9piXEFtN8vYK+JfuEpEvfYCJdOz8Q1WM7TbIpdlRHSIyFD4wpfkneadDDGLzu/TD0o8IttE30tww+7cKjgwBfd1DIFtizENWZJl36p1l8eh1bfks0Ri5l8jVZuPsAoA12ZfdKMpYrldEoe+c7m522y7kkIp/daoxGBee7X4vePMN4tFBBpKqLnBsOXZ3wL/sqxLDVLbYKs8AL8rIJxXSHwC7+tBrXaIch7ZEqN7yOeNtWjRIEDoJIbS/V1+qS9vzMT75Ak6CjWwPGmtPM+t5JwDIpn8NmpM/lM12suukWBpdKBlHqfBMu6t8YEXrDv59Sez3iSkxs7dsHIgyWFl0mEHRyIxFbNkUR4fAkqGR1n7Jms4sg5BSMWlB00fAFpuwj8Ykl/d/tDCKaalrOCyGjq84VR6v71EmkSmvUq5DQOIM23LtvhgwymEHaJy23Yf4a85vvcx5V3ZOF8apnWcbuVzPH0Ip7iAJF+CgHUD034ElGd9UM0lIpMLpKTzmH36JZbJb4fIEop7fLiQ6FMZt++jn+RlBnx9pPx7VHW/kRO/a5Zrq89vCXKZZZysNPyQiDqulJjyZ1dwwiSBc7Ryxbg0t3gZjKSUpetwCGkyX89dJgyjqRH7zhIolMh/n3vfWfQTL7Xds4XcWFITLKLnIGtPqPKft073mq/q4hbkFefOtY7C5ZmU8t37RevmL41++O2s+0RrMmezPJkX3y0rHiBF+9tQMk2n/CEKg7BCjtRPvGTSfUiiSUs1PopoEExC+GiyQ+qZwxfWGmwu7YjYoLtshji6zTvon5HicJ6BTb1Us9FIYvydBVAqRZGU3DvCZiTBFdviDp/ogVOfch2YJaPN+d2+9Q2UpPOLG0pMmHaVYsJcK/Gq7//wbQhqjmYZhpQF4SygJ9PVYTCoJACMqPpxcQLyBwmlsGwiMyETp8wgR+Lrke2ysVcka3WOpo4RJI9n4hwWIO0V0oPZta3eqsCVg1gGqSHO4YX3oc7xUtU79w+aop0+/PwVwlkZGFVrf4Hyc10SlTwmfEFL9qMlZfZgwiJH8yXAK5I/F6W+L+iM25TtkZ4nmxIMDoYJtXTDQEgERzVWiX7tjoHNfxKJi1SSzU2q/TAvh+4gMLVcvcpctZRTmHU0l/KASI9V8lrdBNtyVVNr0qKou/hrAgl0+0TW+1HsEAaDPsohK4iLz+qEZImVQJUnevhUp+TnWnOVTHnrbLqKZgr8aGolXrYQ/yRl8pttJyFsTCge6VELMQZ4hDD9pAvNDOnne2Zl3iRNXDUdEJ9THFFQk7t7Pd4Mnsat1v2FPeAHmJ6ftcOSQn2ou5IUAAkwcZGeQyFAm0pcYBs+RYlU93wT";
-const _U = "1N6XdZoEjrs9bNG3W07jM2krHSUQ0vYNaIAvNGbFVnz7xSZZSDfDUTqYW3g7IEKvGoxieQVFUmX8xBnN5O10gBbCENlfZiY5GPbPlyAwaPUM7lRzYXm3sFtxoQjyZEVcBCwgT-pLt9L8_hVtK2L_CQzjIdgfLIPKhWZymDiBa356Y31UQ_0A1wXZTpVXrN2tMct54TIh3q6Lvf-Y8ucNDI6M3NVjK46oOOi4bZEX43sI";
 
-module.exports.config = {
-	name: 'bing',
-  version: '2.0.0',
-  credits: 'cliff'//api by samir
-  aliases: ['genpro'],
-  description: '𝗗𝗔𝗟𝗟𝗘 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗆𝖺𝖽𝖾 𝖻𝗒 𝖢𝗅𝗂𝖿𝖿 𝖵𝗂𝗇𝖼𝖾𝗇𝗍 𝖳𝗈𝗋𝗋𝖾𝗏𝗂𝗅𝗅𝖺𝗌 𝗂𝗌 𝗎𝗌𝖾 𝗍𝗈 𝗀𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖺𝗂 𝗉𝗂𝖼𝗍𝗎𝗋𝖾𝗌 𝗎𝗌𝗂𝗇𝗀 𝗍𝖾𝗑𝗍',
-  role: 0
-};
+const KievRPSSecAuth = "FAByBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACPdEWecX9G/OMAQ2nuMaOCul+CDMdOHAACpuCIh5U8btXkpIavLSnFJmvhw7unH4LdSog9rOSNi3ud5GDzQzFaN/0zpUBdAaiDZ+moU8XXTncvvrXgU8Poabcue80cT6H1dW6S3OPJAPQi2+dcwdWsdwlRS/PItdgb8/qytPGyoAF+ERFSVb6iR5ocsV52kN+BqjAGfUEYHS2FR1i4cNanwgZxH+lpxqfkKY5mgHmWlPbDBGWN/4nWbcVvwRZq4YnAMHj1lup5EEKwES5bHMfYvpQ62bqA7k1t8NbmDqoEj08Ap1StgXadTDqoqoQSyNP83JXXtGlBMqrPgquyqD50SOFCYDqyv9U3X1PzTEVTdW3KAqcYAsHv6DScT//n7B2DksZYv9Bsg0BMYPsIp5DPjjTrtGCTNagaufXHkaYrOyDq40fbDIyhrh4wJQoNmTPmloNFl9OqL/5ZWuPgOafC7+yDFhmqGOWImaNpU2WejvGvD3P9IZN4Z1f36XSJJIO4pZKAhhwWFkefa3LN8fAYnsJ+WkjDSXAGTuwN6NnW9wzGWnFaD4ojKunjPazYt5qLtlk06PYoqDE6V/MSKxIU2+GVg7gMtBNDMwEKD+pMHB0jVjzx0SyeEYc8m6K/ThfrBGXdwzQWsEiMxP5cXAVLClGDPXRI2h2W6B/n9UGPi8utG7Sz/dV1QaKEtyJPohuO7Onk42jLZ0/EabwpJ7jwaiSYv2AIW+GKO8wRPRlt8nOSZxqCPDKOVgn4f9VoaudyjRPAM7KoiGJ7PhXgMYoqk6ZvmLpcPaWNoH3p7TbKgkO0fd811/3weWZ4dicF4bazE2++QD+wKhXqjiHJZrhJOEOTB1GpZ/wnXSf0pY7SG+qyJcHTr6PJ3l+yv0KKVPCzuv+kg3566u7EzAkZPy7N9bpljQ0UcjloH7FLz2MPo0ZocPvEbQohHE8PFf+0fh7GbtjzHoyfqjQBVeNfQL7s7VSEdOOoluAirLoZeBFWA+JVorvWXpRi/jpTMk45DX1TqZJAVWCWdn6d2yddmZ03CoyWR0xqfYqD69LCOxXBTdz54SsAmPZ+vjLByqlSakKFFSR5WBsJSdY7Hw0uBXunyHBJD0ERh5/igQYP5JwdbWbWvToeVFChimOSJ0iK8r3cEzk4yxiA+JD2KhbR/9TkW5yLHrn/NVvDL9nLSWUw2wWLFFU1iMAiL9E3SVjtw1odHJYhr+7JLBRqp2tBxb1DRiFftu/XyN2oXKLxpX7RzNFH6CD4ywgQ2q4aiQwKeOfvklWovQ/3LyrQn08/mVZ+cn6cMCzUAYMdzYT1OgfhLkYMy0w+zXxINAPkkNZD2ZKgWeJPMW/uBY1DHahr+DSX/vuh1WH6ABAJFl+Hn71BGI+AbjZA0uCqDOKQLfwBGBYsGDBXRfxlKMCf+QnGnPgjQgPXGA820kWxK8FACZ1lFF7mxJUEOcTFrjijsYKfwWLw==";
+const _U = "1blK93hKnYixhtwAtPdb3D8pFjTaKhmfhX5Z5ZE9dk-moahnidxLq97WCyxPXOsrZRrXOkAzyFh4LLCNUHgOHZJLpfF0hM23IrCUkYktZIv7xLUTHSeMHliuqQiWoWboYEgCvR5P1EAFpsFRhEo5l3gNwsFwfSFrzcxHp7roT6rH_4UuiMhvDo3S99XpinYvIA5Dmlu-mtzTyuvRb3J3kn5ALdabtwZDLhy9mr9JMVzE";
 
-module.exports.run = async function ({ api, event, args }) {
-	const keySearch = args.join(" ");
-	const indexOfHyphen = keySearch.indexOf('-');
-	const keySearchs = indexOfHyphen !== -1 ? keySearch.substr(0, indexOfHyphen).trim() : keySearch.trim();
-	const numberSearch = parseInt(keySearch.split("-").pop().trim()) || 4;
+module.exports = {
+  config: {
+    name: "bing",
+    aliases: ["bing2"],
+    version: "1.0.2",
+    role: 1,
+    countDown: 60,
+    shortDescription: {
+      en: "dalle 3 > TsantaBot > Bing"
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "dalle3 pro",
+    guide: {
+      en: "{prefix}bing <search query> -<number of images>"
+    }
+  },
 
-	try {
-		const res = await axios.get(`https://api-dalle-gen.onrender.com/dalle3?auth_cookie_U=${_U}&auth_cookie_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(keySearchs)}`);
-		const data = res.data.results.images;
+  run: async function ({ api, event, args }) {
+    const uid = event.senderID;
+    const permission = [`${uid}`];
+    if (!permission.includes(event.senderID)) {
+      api.sendMessage(
+        "You don't have enough permission to use this command. Only admin and premium can do it.",
+        event.threadID,
+        event.messageID
+      );
+      return;
+    }
 
-		if (!data || data.length === 0) {
-			api.sendMessage("No images found for the provided query.", event.threadID, event.messageID);
-			return;
-		}
+    const keySearch = args.join(" ");
+    const indexOfHyphen = keySearch.indexOf('-');
+    const keySearchs = indexOfHyphen !== -1 ? keySearch.substr(0, indexOfHyphen).trim() : keySearch.trim();
+    const numberSearch = parseInt(keySearch.split("-").pop().trim()) || 4;
 
-		const imgData = [];
-		for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
-			const imgResponse = await axios.get(data[i].url, { responseType: 'arraybuffer' });
-			const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
-			await fs.outputFile(imgPath, imgResponse.data);
-			imgData.push(fs.createReadStream(imgPath));
-		}
+    try {
+      api.sendMessage("⏳ | TsantaBot et Bing sont en train d'imaginer votre textes...", event.threadID, event.messageID); // Added message here
 
-		await api.sendMessage({
-			attachment: imgData,
-			body: `Here's your generated image`
-		}, event.threadID, event.messageID);
+      const res = await axios.get(`https://api-dalle-gen.onrender.com/dalle3?auth_cookie_U=${_U}&auth_cookie_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(keySearchs)}`);
+      const data = res.data.results.images;
 
-	} catch (error) {
-		console.error(error);
-		api.sendMessage("cookie of the command. Is expired", event.threadID, event.messageID);
-	} finally {
-		await fs.remove(path.join(__dirname, 'cache'));
-	}
+      if (!data || data.length === 0) {
+        api.sendMessage("No images found for the provided query.", event.threadID, event.messageID);
+        return;
+      }
+
+      const imgData = [];
+      for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
+        const imgResponse = await axios.get(data[i].url, { responseType: 'arraybuffer' });
+        const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
+        await fs.outputFile(imgPath, imgResponse.data);
+        imgData.push(fs.createReadStream(imgPath));
+      }
+
+      await api.sendMessage({
+        attachment: imgData,
+        body: `✅ | TsantaBot: Voici votre images :`
+      }, event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("🤯 Oh non, Je suis malade ! ", event.threadID, event.messageID);
+    } finally {
+      await fs.remove(path.join(__dirname, 'cache'));
+    }
+  }
 };

5c5ae67873e07c5b16f14c338381b2f4fa42ee38
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-02-12 17:27:09 +0300

    Add files via upload

---
+++ script/llama.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+module.exports.config = {
+ name: 'koto',
+ credits: "cliff",
+  version: '1.0.0',
+  role: 0,
+  aliases: ['llma'],
+  cooldown: 20,
+  hasPrefix: true,
+  usage: "",
+};
+
+module.exports.run = async function ({ api, event, args }) {
+ try {
+ const { messageID, messageReply } = event;
+ let prompt = args.join(' ');
+
+ if (messageReply) {
+  const repliedMessage = messageReply.body;
+  prompt = `${repliedMessage} ${prompt}`;
+ }
+
+ if (!prompt) {
+  return api.sendMessage('- Koto [Question]\n 🌐 bit.ly/tsantabot', event.threadID, event.messageID);
+ }
+
+ const llama_api = `https://llama.aliestercrowley.com/api?prompt=${encodeURIComponent(prompt)}`;
+
+ const response = await axios.get(llama_api);
+
+ if (response.data && response.data.response) {
+  const generatedText = response.data.response;
+  api.sendMessage({ body: generatedText, attachment: null }, event.threadID, messageID);
+ } else {
+  console.error('API response did not contain expected data:', response.data);
+  api.sendMessage('❌ An error occurred while generating the text response. Please try again later.', event.threadID, messageID);
+ }
+ } catch (error) {
+ console.error('Error:', error);
+ api.sendMessage('❌ An error occurred while generating the text response. Please try again later.', event.threadID, messageID);
+ }
+};

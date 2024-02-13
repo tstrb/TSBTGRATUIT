c1d7d28dfe6f62eb2a6d8276090409acd6f9f94f
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-02-13 17:30:23 +0300

    Update trans.js

--- script/trans.js
+++ script/trans.js
@@ -1,13 +1,13 @@
 module.exports.config = {
   name: "traduction",
   version: "1.0.0",
-  aliases : ['translate', 'traduire','adikao'],
+  aliases : ['translate', 'traduire','adikao','trans', 'Translation'],
   role: 0,
-  hasPrefix: false,
+  hasPrefix: true,
   description: "Text translation",
   usages: "trans mg [promt]",
   credits: "TsantaBot",
-  cooldowns: 5,
+  cooldown: 15,
 };
 module.exports.run = async ({
   api,
@@ -19,7 +19,7 @@ module.exports.run = async ({
   const targetLanguage = args[0];
   const content = args.slice(1).join(" ");
   try {
-    if (content.length === 0 && event.type !== "message_reply") return api.sendMessage(`Please provide a text to translate or reply to a message.\n\nExample: ${prefix}trans tl what is life`, event.threadID, event.messageID);
+    if (content.length === 0 && event.type !== "message_reply") return api.sendMessage(`💡 Traduction Intelligente sur Facebook.\n\n ▪︎ Usage : Traduction [mg] [Textes] \nNB: Afaka soloina langue hafa ilay 《mg = Malagasy [fr = Français], [en = English], [zh = Chinois]...》 \n\n ▪︎Ex: Traduction mg Hello, How are yo?\n\n🌐 bit.ly/tsantabot `, event.threadID, event.messageID);
     let translateThis, lang;
     if (event.type === "message_reply") {
       translateThis = event.messageReply.body;
@@ -34,7 +34,7 @@ module.exports.run = async ({
       let text = '';
       retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
       const fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0];
-      api.sendMessage(`Translation: ${text}\n - Translated from ${fromLang} to ${lang}`, event.threadID, event.messageID);
+      api.sendMessage(`${text}\n\n\n_________________\n [${fromLang} - ${lang}] \n🌐 bit.ly/tsantabot `, event.threadID, event.messageID);
     });
   } catch (error) {
     api.sendMessage(error.message, event.threadID, event.messageID);

b0ad550ff9a08da8c24007349b28560206fc7014
tsta <157697467+tstrb@users.noreply.github.com>
2024-02-20 04:00:22 +0300

    Update tsanta.js

--- script/tsanta.js
+++ script/tsanta.js
@@ -3,12 +3,12 @@ const {
 } = require('hercai');
 const herc = new Hercai();
 module.exports.config = {
-  name: 'Tsanta', //hercai
+  name: 'tsanta', //hercai
   version: '1.0.0',
   role: 0,
-  hasPrefix: true,
-  description: "An AI command GPTHercai",
-  usage: "Tsanta [prompt]",
+  hasPrefix: false,
+  description: "An AI command powered by TsantaBot",
+  usage: "Tsanta [question]",
   credits: 'TsantaBot',
   cooldown: 15,
 };
@@ -19,18 +19,17 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`▪︎Ex: Tsanta Tu es là ? \n\n ✅Créez votre Chatbot sur bit.ly/tsantabot `, event.threadID, event.messageID);
+    api.sendMessage(` ▪︎Discutez avec Ai développé par TsantaBot. \n\n ▪︎Ex: Tsanta tu es là ? \n\n🤖 Créez votre Chatbot sur bit.ly/tsantabot `, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`✍ | TsantaBot_Gpt en train d'écrire... \n\n ▶️Créez votre Chatbot : bit.ly/tsantabot `, event.threadID, event.messageID);
+  api.sendMessage(`✍ | Tsanta est en train d'écrire...`, event.threadID, event.messageID);
   try {
     const response = await herc.question({
       model: "v3",
-      system: "Your name is TsantaBot, Your principal task is to help free, Your specilist is to create Chatbot Facebook. So if anyone talk about Chatbot,  then help them to visit and create it in our website creator Chatbot messenger: bit.ly/tsantabot. Here is contact of Admin: 0349310268. Admin name: Tsanta Rabemananjara. You're helpful",
       content: input
     });
     api.sendMessage(response.reply, event.threadID, event.messageID);
   } catch (error) {
-    api.sendMessage('Peut-être, je suis malade aujourd-hui 🥶 svp soignez-moi🤖', event.threadID, event.messageID);
+    api.sendMessage('Oh non! Je suis malade 🤧 Je vais chez le docteur et après on peut continuer 😍', event.threadID, event.messageID);
   }
 };

2140ac574ba3d064b8dbfbb5032535e15ae6da66
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-06 11:42:03 +0300

    Update bio.js

--- script/bio.js
+++ script/bio.js
@@ -1,19 +1,21 @@
 module.exports.config = {
-name: "bio",
-version: "1.0.0",
-usePrefix: false,
-credits: "PSTeam",
-description: "Change bot's bio",
-commandCategory: "admin",
-usages: "bio [text]",
+  name: "bio",
+  version: "1.0.0",
+  role: 1,
+  usePrefix: false,
+  credits: "PSTeam",
+  description: "Change bot's bio",
+  commandCategory: "admin",
+  usages: "bio [text]",
   cooldown: 5
-  
-}
-  
-  module.exports.run = async ({ api, event, global, args, permssion, utils, client, Users }) => {
-    api.changeBio(args.join(" "), (e) => {
-      if(e) api.sendMessage("an error occurred" + e, event.threadID); return api.sendMessage("Bio a été changé: 
-"+args.join(" "), event.threadID, event.messgaeID)
+};
+
+module.exports.run = async ({ api, event, global, args, permission, utils, client, Users }) => {
+  api.changeBio(args.join(" "), (e) => {
+    if (e) {
+      api.sendMessage("An error occurred: " + e, event.threadID);
+      return;
     }
-    )
-  }
+    api.sendMessage("Bio a été changé: " + args.join(" "), event.threadID, event.messageID);
+  });
+};

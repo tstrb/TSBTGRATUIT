95b8739284c98dd68f0b5af97391744a0c4e7ac7
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-06 13:17:54 +0300

    Update music.js

--- script/music.js
+++ script/music.js
@@ -1,86 +1,60 @@
+const path = require('path');
 module.exports.config = {
   name: "music",
   version: "1.0.0",
   role: 0,
-  usePrefix: true,
-  credits: "fk",
-  description: "Play music from youtube",
-  commandCategory: "utility",
-  usages: "[title]",
-  cooldown: 10,
-  dependencies: {}
+  hasPrefix: true,
+  aliases: ['play'],
+  usage: 'Music [promt]',
+  description: 'Search music in youtube',
+  credits: 'Deveploper',
+  cooldown: 5
 };
-
-module.exports.run = async ({ api, event }) => {
-  const axios = require("axios");
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
   const fs = require("fs-extra");
-  const Innertube = require("youtubei.js");
-  const request = require("request");
-  let input = event.body;
-
-  var text = input;
-  text = text.substring(7);
-  let data = input.split(" ");
-
-  if (data.length < 2) {
-    return api.sendMessage("⚠️Please put a title or name of the music.", event.threadID);
+  const ytdl = require("ytdl-core");
+  const yts = require("yt-search");
+  const musicName = args.join(' ');
+  if (!musicName) {
+    api.sendMessage(`To get started, type music and the title of the song you want.`, event.threadID, event.messageID);
+    return;
   }
-
-  data.shift();
-
-  const youtube = await new Innertube();
-
-  const search = await youtube.search(text);
-  if (search.videos[0] === undefined) {
-    api.sendMessage("Error: Invalid request.", event.threadID, event.messageID);
-    api.setMessageReaction("❎", event.messageID, (err) => {}, true);
-  } else {
-    api.sendMessage(`🔎Searching for "${text}"...`, event.threadID, event.messageID);
-    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-    var timeleft = 3;
-    var downloadTimer = setInterval(function () {
-      if (timeleft <= 0) {
-        clearInterval(downloadTimer);
-      }
-      timeleft -= 1;
-    }, 1000);
-    const stream = youtube.download(search.videos[0].id, {
-      format: "mp4",
-      type: "audio",
-      audioQuality: "lowest",
-      loudnessDB: "20",
-      audioBitrate: "320",
-      fps: "30",
-    });
-
-    stream.pipe(fs.createWriteStream(__dirname + `/cache/${search.videos[0].title}.mp3`));
-
-    stream.on("start", () => {
-      console.info("[DOWNLOADER]", "Starting download now!");
-    });
-    stream.on("info", (info) => {
-      console.info("[DOWNLOADER]", `Downloading ${info.video_details.title} by ${info.video_details.metadata.channel_name}`);
-      console.log(info);
-    });
-
-    stream.on("end", () => {
-      console.info(`[DOWNLOADER] Downloaded`);
-
-      var message = {
-        body: `Here's your music, enjoy!🥰\n\nTitle: ${search.videos[0].title}`,
-        attachment: [fs.createReadStream(__dirname + `/cache/${search.videos[0].title}.mp3`)],
-      };
-      api.sendMessage(message, event.threadID, event.messageID);
-    });
-    stream.on("error", (err) => console.error("[ERROR]", err));
-
-    stream.on("end", async () => {
-      if (fs.existsSync(__dirname + `/cache/${search.videos[0].title}.mp3`)) {
-        fs.unlink(__dirname + `/cache/${search.videos[0].title}.mp3`, function (err) {
-          if (err) console.log(err);
-          console.log(__dirname + `/cache/${search.videos[0].title}.mp3 is deleted!`);
-        });
-      }
-    });
+  try {
+    api.sendMessage(`🔎| TsantaBot cherche 《${musicName}》 `, event.threadID, event.messageID);
+    const searchResults = await yts(musicName);
+    if (!searchResults.videos.length) {
+      return api.sendMessage("Aucun résultat.", event.threadID, event.messageID);
+    } else {
+      const music = searchResults.videos[0];
+      const musicUrl = music.url;
+      const stream = ytdl(musicUrl, {
+        filter: "audioonly"
+      });
+      const time = new Date();
+      const timestamp = time.toISOString().replace(/[:.]/g, "-");
+      const filePath = path.join(__dirname, 'cache', `${timestamp}_music.mp3`);
+      stream.pipe(fs.createWriteStream(filePath));
+      stream.on('response', () => {});
+      stream.on('info', (info) => {});
+      stream.on('end', () => {
+        if (fs.statSync(filePath).size > 26214400) {
+          fs.unlinkSync(filePath);
+          return api.sendMessage('The file could not be sent because it is larger than 25MB.', event.threadID);
+        }
+        const message = {
+          body: `${music.title}`,
+          attachment: fs.createReadStream(filePath)
+        };
+        api.sendMessage(message, event.threadID, () => {
+          fs.unlinkSync(filePath);
+        }, event.messageID);
+      });
+    }
+  } catch (error) {
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
 };

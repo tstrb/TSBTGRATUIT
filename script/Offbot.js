a28a497fb206bb767ccc2f3ff924cbad32024bcc
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-14 14:15:53 +0300

    Create Offbot.js

---
+++ script/Offbot.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+	name: "offbot",
+	version: "1.0.0",
+	usePrefix: 1,
+	credits: "HTHB",
+	description: "turn the bot off",
+	commandCategory: "system",
+	cooldown: 10
+        };
+module.exports.run = ({event, api}) =>{
+    const permission = [`61552825191002`];
+	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command.\nOnly LaFhanGa chokra", event.threadID, event.messageID);
+  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+}
